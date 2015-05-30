ac049644fae4dfe1d98135ba9e5fe020feaaa0fc
Hugues Stefanski <hugues.stefanski@gmail.com>
2015-05-30 12:35:04 +0200

    Refactoring of axes Layout of axes correct

--- dist/charting.js
+++ dist/charting.js
@@ -4,30 +4,42 @@ var charting;
 (function (charting) {
     var chart = (function () {
         function chart(container) {
+            this._paddingLeft = 30;
+            this._paddingBottom = 30;
             this.init(container);
         }
         chart.prototype.init = function (container) {
-            this._group = d3.select(container).append('g');
-            this.draw();
+            var selection = d3.select(container);
+            var width = selection.node().clientWidth;
+            var height = selection.node().clientHeight;
+            this._height = height;
+            this._group = selection.append('svg').attr({
+                'width': width,
+                'height': height
+            }).append('g');
+            this.draw(width, height);
         };
-        chart.prototype.draw = function () {
-            this.drawXAxis();
-            this.drawYAxis();
+        chart.prototype.draw = function (width, height) {
+            this.drawXAxis(width);
+            this.drawYAxis(height);
         };
-        chart.prototype.drawXAxis = function () {
+        chart.prototype.drawXAxis = function (width) {
             var scale = d3.scale.linear();
             scale.domain([0, 1]);
-            scale.range([0, 600]);
+            scale.range([0, width - this._paddingLeft]);
             var xAxis = d3.svg.axis().scale(scale).orient('bottom');
-            this._group.append('g').call(xAxis);
+            this._group.append('g').call(xAxis).attr({
+                'transform': 'translate(' + this._paddingLeft + ',' + (this._height - this._paddingBottom) + ')'
+            });
         };
-        chart.prototype.drawYAxis = function () {
+        chart.prototype.drawYAxis = function (height) {
+            var paddingLeft = 30;
             var scale = d3.scale.linear();
             scale.domain([0, 1]);
-            scale.range([800, 0]);
+            scale.range([height - this._paddingBottom, 0]);
             var axis = d3.svg.axis().scale(scale).orient('left');
             this._group.append('g').call(axis).attr({
-                'transform': 'translate(30 0)'
+                'transform': 'translate(' + this._paddingLeft + ', 0)'
             });
         };
         return chart;
--- src/axis.ts
+++ src/axis.ts
@@ -3,46 +3,63 @@
 module charting {
 	export class chart {
 		private _group: D3.Selection;
-
+		private _paddingLeft = 30;
+		private _paddingBottom = 30;
+		private _height: number;
 		constructor(container: any) {
 			this.init(container);
 		}
 		
 		private init(container) {
-			this._group = d3.select(container).append('g');
-			this.draw();
+			var selection = d3.select(container);
+			
+
+                var width = selection.node().clientWidth;
+                var height = selection.node().clientHeight;	
+				this._height = height;
+				this._group = selection.append('svg')
+					.attr({
+						'width': width,
+						'height':height
+					})	
+					.append('g');
+			this.draw(width, height);
 		}
 
-		draw() {
-			this.drawXAxis();
-			this.drawYAxis();
+		draw(width: number, height: number) {
+			this.drawXAxis(width);
+			this.drawYAxis(height);
 		}
 		
-		private drawXAxis() {
+		private drawXAxis(width) {
 			var scale = d3.scale.linear();
 
 			scale.domain([0, 1]);
-			scale.range([0, 600]);
+			scale.range([0, width-this._paddingLeft]);
 			var xAxis = d3.svg.axis()
 				.scale(scale)
 				.orient('bottom');
-			this._group.append('g').call(xAxis);
+			this._group.append('g')
+				.call(xAxis)
+				.attr({
+				'transform':'translate('+this._paddingLeft+','+(this._height-this._paddingBottom)+')'
+				});
 			
 		}
-		private drawYAxis() {
+		private drawYAxis(height: number) {
+			var paddingLeft = 30;
 			var scale = d3.scale.linear();
 
 			scale.domain([0, 1]);
-			scale.range([800, 0]);
+			scale.range([height-this._paddingBottom, 0]);
 			var axis = d3.svg.axis()
 				.scale(scale)
 				.orient('left');
 			this._group.append('g')
 				.call(axis)
 				.attr({
-				'transform': 'translate(30 0)'
+				'transform': 'translate('+this._paddingLeft+', 0)'
 			});
-			
 		}
 	}
 }
